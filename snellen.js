15fbc1935cc1aeaeea36ede747f544932b7e6d0c
coote-andrew <coote.andrew@gmail.com>
2023-06-27 18:56:34 +1000

    Update snellen.js
    testing swipe functions

--- snellen.js
+++ snellen.js
@@ -58,6 +58,24 @@
         changeWithKey(event);
     });
 
+
+    let touchstartX = 0
+    let touchendX = 0
+    
+    function checkDirection() {
+      if (touchendX < touchstartX) alert('swiped left!')
+      if (touchendX > touchstartX) alert('swiped right!')
+    }
+
+    document.addEventListener('touchstart', e => {
+      touchstartX = e.changedTouches[0].screenX
+    })
+
+    document.addEventListener('touchend', e => {
+      touchendX = e.changedTouches[0].screenX
+      checkDirection()
+    })
+
     document.addEventListener('swiped-right', function(e){
         changeLetter();
     })
