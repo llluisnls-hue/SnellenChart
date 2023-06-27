4c873e53163b649f811d4ddab1c439430a47dfe6
coote-andrew <coote.andrew@gmail.com>
2023-06-27 21:13:41 +1000

    Update snellen.js

--- snellen.js
+++ snellen.js
@@ -102,7 +102,7 @@
     });
 
     function showHideFixHeight(){
-        if(currentHeightInput.getPropertyValue(display) == "none"){
+        if(currentHeightInput.getPropertyValue("display") == "none"){
             currentHeightInput.style.display="block"
         }
             
